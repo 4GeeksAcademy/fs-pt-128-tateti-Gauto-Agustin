d82fdddd2f27476d0495ff48326f15de977dbadf
AgustinMGauto <agustin_gauto@outlook.com>
2026-01-06 19:58:17 +0000

    boton reset a medias

--- src/app.js
+++ src/app.js
@@ -2,16 +2,48 @@ import "bootstrap";
 import "./style.css";
 
 
-let startingBoard = ["","","","","","","","",""]
+let startingBoard = ["", "", "", "", "", "", "", "", ""]
 let actualPlayer = "X"
 
+
+let gameContinue = true;
+const winningOptions = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8],
+  [0, 3, 6], [1, 4, 7], [2, 5, 8],
+  [0, 4, 8], [2, 4, 6]
+]
+
+function resultWinner() {
+  let roundWon = false;
+  for (let i = 0; i < winningOptions.length; i++) {
+    const [a, b, c] = winningOptions[i];
+    if (startingBoard[a] === "" || startingBoard[b] === "" || startingBoard[c] === "") continue;
+    if (startingBoard[a] === startingBoard[b] && startingBoard[b] === startingBoard[c]) {
+      roundWon = true;
+      break;
+    }
+  }
+  if (roundWon) {
+    document.getElementById("status-game").innerHTML = `¡Ganó ${actualPlayer}!`
+    gameContinue = false;
+    return;
+  }
+  if (!startingBoard.includes("")) {
+    document.getElementById("status-game").innerHTML = "¡Empate!"
+    gameContinue = false;
+  }
+}
+
+
 document.querySelectorAll(".grid").forEach(element => {
-  element.addEventListener("click", ()=>{
+  element.addEventListener("click", () => {
     let index = element.getAttribute("data-index")
-    if(startingBoard[index] != "") return
+    if (startingBoard[index] !== "" || !gameContinue) return;
+    if (startingBoard[index] != "") return
     startingBoard[index] = actualPlayer
     element.innerHTML = actualPlayer
-    if (actualPlayer == "X"){
+    resultWinner();
+    if (actualPlayer == "X") {
       actualPlayer = "O"
     } else {
       actualPlayer = "X"
@@ -21,3 +53,13 @@ document.querySelectorAll(".grid").forEach(element => {
   })
 })
 
+
+function reset() {
+    startingBoard = ["", "", "", "", "", "", "", "", ""];
+    actualPlayer = "X";
+    gameContinue = true;
+
+    
+}
+
+document.getElementById("reset-button").addEventListener("click", reset);
